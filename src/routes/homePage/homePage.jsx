7fed933c6e3fa426728dffe090ed10a15523e303
Markiian Kostiv <markiian.kostiv@gmail.com>
2024-06-02 16:27:01 +0300

    MK_homePage_33-44 Create homePage component

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -1,5 +1,41 @@
-import './homePage.scss'
+import "./homePage.scss";
 
 export default function HomePage() {
-  return <div>HomePage</div>
+  return (
+    <div className='homePage'>
+      <div className='textContainer'>
+        <div className='wrapper'>
+          <h1 className='title'>Find Real Estate & Get Your Dream</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+            explicabo suscipit cum eius, iure est nulla animi consequatur
+            facilis id pariatur fugit quos laudantium temporibus dolor ea
+            repellat provident impedit!
+          </p>
+          {/* Search bar */}
+          <div>Search Bar</div>
+          <div className='boxes'>
+            <div className='box'>
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className='box'>
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className='box'>
+              <h1>1200+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='imgContainer'>
+        <img
+          src='/bg.png'
+          alt=''
+        />
+      </div>
+    </div>
+  );
 }
