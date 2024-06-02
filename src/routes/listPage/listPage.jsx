0dbd920167e5deb35b6d5885603388f527c4d802
Богдан <brazbogdan@gmail.com>
2024-06-02 21:33:34 +0300

    Створив ListPage

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,5 +1,20 @@
+import { listData } from '../../lib/dummydata'
 import './listPage.scss'
+import Filter from '../../components/filter/Filter';
+import Card from '../../components/card/Card';
 
-export default function ListPage() {
-  return <div>ListPage</div>
+ function ListPage() {
+  const data = listData;
+  return <div className='listPage'>
+  <div className="listContainer">
+    <div className="wrapper">
+      <Filter></Filter>
+      {data.map(item=>(
+        <Card key={item.id} item={item}></Card>
+        ))}
+    </div>
+  </div>
+  <div className="mapContainer"></div>
+  </div>
 }
+export default ListPage;
