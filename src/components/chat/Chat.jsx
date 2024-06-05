1c12205939476d2bdc287776e3f48e95281e39d6
Brodich Vlad <brodich_vlad@ukr.net>
2024-06-05 18:09:59 +0300

    create component chat::VB

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -7,6 +7,7 @@ export default function Chat() {
   return <div className='chat'>
     <div className='messages'>
       <h1>Messages</h1>
+
       <div className='message'>
         <img src='https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='photo John Doe'/>
         <span>John Doe</span>
@@ -14,6 +15,7 @@ export default function Chat() {
           Lorem ipsum dolor, sit amet...
         </p>
       </div>
+
       <div className='message'>
         <img src='https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='photo John Doe'/>
         <span>John Doe</span>
@@ -21,6 +23,7 @@ export default function Chat() {
           Lorem ipsum dolor, sit amet...
         </p>
       </div>
+
       <div className='message'>
         <img src='https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='photo John Doe'/>
         <span>John Doe</span>
@@ -28,6 +31,7 @@ export default function Chat() {
           Lorem ipsum dolor, sit amet...
         </p>
       </div>
+
       <div className='message'>
         <img src='https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='photo John Doe'/>
         <span>John Doe</span>
@@ -35,6 +39,7 @@ export default function Chat() {
           Lorem ipsum dolor, sit amet...
         </p>
       </div>
+
       <div className='message'>
         <img src='https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='photo John Doe'/>
         <span>John Doe</span>
@@ -42,54 +47,68 @@ export default function Chat() {
           Lorem ipsum dolor, sit amet...
         </p>
       </div>
+
     </div>
+
     {chat && 
-    <div className='chatBox'>
-      <div className='top'>
-        <div className='user'>
-          <img src='https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='photo John Doe' />
-          John Doe
+      <div className='chatBox'>
+
+        <div className='top'>
+          <div className='user'>
+            <img src='https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='photo John Doe' />
+            John Doe
+          </div>
+          <span className='close' onClick={()=>{setChat(null)}}>X</span>
         </div>
-        <span className='close' onClick={()=>{setChat(null)}}>X</span>
+
+        <div className='center'>
+
+          <div className='chatMessage'>
+            <p>Lorem ipsum, dolor sit amet</p>
+            <span>1 hour ago</span>
+          </div>
+
+          <div className='chatMessage own'>
+            <p>Lorem ipsum, dolor sit amet</p>
+            <span>1 hour ago</span>
+          </div>
+
+          <div className='chatMessage'>
+            <p>Lorem ipsum, dolor sit amet</p>
+            <span>1 hour ago</span>
+          </div>
+
+          <div className='chatMessage own'>
+            <p>Lorem ipsum, dolor sit amet</p>
+            <span>1 hour ago</span>
+          </div>
+
+          <div className='chatMessage'>
+            <p>Lorem ipsum, dolor sit amet</p>
+            <span>1 hour ago</span>
+          </div>
+
+          <div className='chatMessage own'>
+            <p>Lorem ipsum, dolor sit amet</p>
+            <span>1 hour ago</span>
+          </div>
+
+          <div className='chatMessage'>
+            <p>Lorem ipsum, dolor sit amet</p>
+            <span>1 hour ago</span>
+          </div>
+
+          <div className='chatMessage own'>
+            <p>Lorem ipsum, dolor sit amet</p>
+            <span>1 hour ago</span>
+          </div>
+        </div>
+        
+        <div className='bottom'>
+          <textarea></textarea>
+          <button>Send</button>
+        </div>
       </div>
-      <div className='center'>
-        <div className='chatMessage'>
-          <p>Lorem ipsum, dolor sit amet</p>
-          <span>1 hour ago</span>
-        </div>
-        <div className='chatMessage own'>
-          <p>Lorem ipsum, dolor sit amet</p>
-          <span>1 hour ago</span>
-        </div>
-        <div className='chatMessage'>
-          <p>Lorem ipsum, dolor sit amet</p>
-          <span>1 hour ago</span>
-        </div>
-        <div className='chatMessage own'>
-          <p>Lorem ipsum, dolor sit amet</p>
-          <span>1 hour ago</span>
-        </div>
-        <div className='chatMessage'>
-          <p>Lorem ipsum, dolor sit amet</p>
-          <span>1 hour ago</span>
-        </div>
-        <div className='chatMessage own'>
-          <p>Lorem ipsum, dolor sit amet</p>
-          <span>1 hour ago</span>
-        </div>
-        <div className='chatMessage'>
-          <p>Lorem ipsum, dolor sit amet</p>
-          <span>1 hour ago</span>
-        </div>
-        <div className='chatMessage own'>
-          <p>Lorem ipsum, dolor sit amet</p>
-          <span>1 hour ago</span>
-        </div>
-      </div>
-      <div className='bottom'>
-        <textarea></textarea>
-        <button>Send</button>
-      </div>
-    </div>}
+    }
   </div>
 }
