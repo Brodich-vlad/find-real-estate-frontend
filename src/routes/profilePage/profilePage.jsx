1cbc760d88a68d1b5a7a67a45d6846168b1c5887
kseniia <104863533+roi5430@users.noreply.github.com>
2024-06-06 11:35:00 +0300

    added profile page

--- src/routes/profilePage/profilePage.jsx
+++ src/routes/profilePage/profilePage.jsx
@@ -1,9 +1,49 @@
-import './profilePage.scss'
+import List from "../../components/list/List";
+import "./profilePage.scss";
 
 function ProfilePage() {
   return (
-	<div>profilePage</div>
-  )
+    <div className="profilePage">
+      <div className="details">
+        <div className="wrapper">
+          <div className="title">
+            <h2>User Information</h2>
+            <button>Updete Profile</button>
+          </div>
+          <div className="info">
+            <span>
+              Avatar:
+              <img
+                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt="user photo"
+              />
+            </span>
+            <span>
+              Username:
+              <b>John Doe</b>
+            </span>
+            <span>
+              Email:
+              <b>john@gmail.com</b>
+            </span>
+          </div>
+          <div className="title">
+            <h2>My List</h2>
+            <button>Create New Post</button>
+          </div>
+          <List />
+          <div className="title">
+            <h2>Saved List</h2>
+          </div>
+          <List />
+        </div>
+      </div>
+
+      <div className="chatContainer">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
