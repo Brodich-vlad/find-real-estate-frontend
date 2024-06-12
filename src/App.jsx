7b68e9201beb70e6daea0c1e131c5fa415eb5af8
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-06-12 18:12:03 +0200

    login page, register page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import HomePage from "./routes/homePage/homePage.jsx";
 import ListPage from "./routes/listPage/listPage.jsx";
 import SinglePage from "./routes/singlePage/singlePage.jsx";
 import ProfilePage from './routes/profilePage/profilePage.jsx';
+import Register from './components/register/register.jsx';
+import Login from './routes/login/login.jsx';
 
 const router = createBrowserRouter([
   {
@@ -29,6 +31,14 @@ const router = createBrowserRouter([
       {
         path: "/profile",
         element: <ProfilePage/>
+      },
+      {
+        path:"/login",
+        element:<Login/>
+      },
+      {
+        path:"/register",
+        element:<Register/>
       }
     ]
   }
