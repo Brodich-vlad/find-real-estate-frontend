611619963596caf6d3df56a846aab69518063943
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-06-13 19:52:43 +0200

    React JWT Cookie token logout button

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,14 +1,19 @@
-import axios from 'axios'
+
 import './register.scss'
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
+import apiRequest from '../../lib/apiRequest'
 
 function Register () {
   const [error, setError] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
   const navigate = useNavigate()
-  
+
   const handleSubmit = async e => {
     e.preventDefault()
+	setIsLoading(true)
+	setError('')
+
     const formData = new FormData(e.target)
 
     const username = formData.get('username')
@@ -16,7 +21,7 @@ function Register () {
     const password = formData.get('password')
 
     try {
-      const res = await axios.post('http://localhost:8800/api/auth/register', {
+      const res = await apiRequest.post('/auth/register', {
         username,
         email,
         password
@@ -26,7 +31,9 @@ function Register () {
     } catch (err) {
       console.log(err)
       setError(err.response.data.message)
-    }
+    } finally{
+		setIsLoading(false)
+	}
   }
   return (
     <div className='register'>
@@ -36,7 +43,7 @@ function Register () {
           <input name='username' type='text' placeholder='Username' />
           <input name='email' type='text' placeholder='Email' />
           <input name='password' type='password' placeholder='Password' />
-          <button>Register</button>
+          <button disabled={isLoading}>Register</button>
           {error && <span>{error}</span>}
           <Link to='/login'>Do you have an account?</Link>
         </form>
