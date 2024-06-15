a080e3863f0660b07fdd122294a660caab7376e6
Brodich Vlad <brodich_vlad@ukr.net>
2024-06-15 14:51:07 +0300

    fix AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import { createContext,useEffect,useState } from "react";
 
 export const AuthContext = createContext();
 
-export const AuthContextProvider=({children})=>{
+export const AuthContextProvider = ({children}) => {
   const[ currentUser, setCurrentUser ] = useState(
     JSON.parse(localStorage.getItem("user")) || null
   );
