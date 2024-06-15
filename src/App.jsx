82cbc031550ba8633e30a531da7644a5b5d5b001
Brodich Vlad <brodich_vlad@ukr.net>
2024-06-15 14:48:52 +0300

    add RequireAuth

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import Layout from './routes/layout/layout.jsx'
+import {Layout, RequireAuth } from './routes/layout/layout.jsx'
 import HomePage from './routes/homePage/homePage.jsx'
 import ListPage from './routes/listPage/listPage.jsx'
 import SinglePage from './routes/singlePage/singlePage.jsx'
@@ -26,16 +26,22 @@ const router = createBrowserRouter([
         element: <SinglePage />
       },
       {
-        path: '/profile',
-        element: <ProfilePage />
-      },
-      {
         path: '/login',
         element: <Login />
       },
       {
         path: '/register',
         element: <Register />
+      },
+      {
+        path: '/',
+        element:<RequireAuth/>,
+        children: [    
+          {
+            path: '/profile',
+            element: <ProfilePage />
+          },
+        ]
       }
     ]
   }
--- src/routes/profilePage/profilePage.jsx
+++ src/routes/profilePage/profilePage.jsx
@@ -3,7 +3,7 @@ import Chat from "../../components/chat/Chat";
 import List from "../../components/list/List";
 import apiRequest from '../../lib/apiRequest';
 import "./profilePage.scss";
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 
 function ProfilePage() {
@@ -11,12 +11,6 @@ function ProfilePage() {
   const { updateUser, currentUser } = useContext(AuthContext)
  
   const navigate = useNavigate()
-
-  useEffect(()=>{
-    if(!currentUser){
-      navigate ("/login")
-    }
-  },[currentUser,navigate])
 
   const handleLogout = async () => {
 
@@ -30,7 +24,7 @@ function ProfilePage() {
   }
 
     return (
-    currentUser &&  <div className="profilePage">
+      <div className="profilePage">
         <div className="details">
           <div className="wrapper">
             <div className="title">
