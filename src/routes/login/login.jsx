6b710184e13153aee33d8965cd2cd9925fc17ff7
Brodich Vlad <brodich_vlad@ukr.net>
2024-06-15 14:18:02 +0300

    add AuthContext

--- src/routes/login/login.jsx
+++ src/routes/login/login.jsx
@@ -1,11 +1,15 @@
 import './login.scss'
 import { Link, useNavigate } from 'react-router-dom'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import apiRequest from '../../lib/apiRequest'
+import { AuthContext } from '../../context/AuthContext'
 
 function Login () {
   const [error, setError] = useState('')
   const [isLoading, setIsLoading] = useState(false)
+
+  const { updateUser } = useContext(AuthContext)
+
   const navigate = useNavigate()
   
   const handleSubmit = async e => {
@@ -23,9 +27,9 @@ function Login () {
         username,
         password
       })
-      console.log(res)
-      
-      localStorage.setItem('user', JSON.stringify(res.data))
+
+      updateUser(res.data)
+
       navigate('/')
     } catch (err) {
       console.log(err)
