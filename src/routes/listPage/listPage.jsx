2eab581e0b25109bee7a356f8850158100c214cc
Svitlana Shkriabina <ssarisoydan@gmail.com>
2024-06-24 01:56:11 +0300

    react fetch data based on url query 03:04-03:13

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,23 +1,26 @@
-import { listData } from '../../lib/dummydata'
-import './listPage.scss'
-import Filter from '../../components/filter/Filter';
-import Card from '../../components/card/Card';
-import Map from '../../components/map/Map';
+import "./listPage.scss";
+import Filter from "../../components/filter/Filter";
+import Card from "../../components/card/Card";
+import Map from "../../components/map/Map";
+import { useLoaderData } from "react-router-dom";
 
- function ListPage() {
-  const data = listData;
-  return <div className='listPage'>
-  <div className="listContainer">
-    <div className="wrapper">
-      <Filter/>
-      {data.map(item=>(
-        <Card key={item.id} item={item}></Card>
-        ))}
+function ListPage() {
+  const posts = useLoaderData();
+
+  return (
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {posts.map((item) => (
+            <Card key={item.id} item={item}></Card>
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">
+        <Map items={posts} />
+      </div>
     </div>
-  </div>
-  <div className="mapContainer">
-    <Map items={data}/>
-  </div>
-  </div>
+  );
 }
 export default ListPage;
