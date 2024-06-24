1a3a56d0b309e4d361a84d7ba6164f3390c79de3
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-06-24 17:51:55 +0200

    skeleton 3.13-3.27

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,17 +1,48 @@
+import { useSearchParams } from 'react-router-dom'
 import "./filter.scss"
+import { useState } from 'react'
 function Filter() {
+
+    const [searchParams, setSearchParams] = useSearchParams()
+    const [query, setQuery] = useState({
+        type: searchParams.get("type") || "",
+        city: searchParams.get("city") || "",
+        property: searchParams.get("property") || "",
+        minPrice: searchParams.get("minPrice") || "",
+        maxPrice: searchParams.get("maxPrice") || "",
+        bedroom: searchParams.get("bedroom") || "",
+      });
+    
+      const handleChange = (e) => {
+        setQuery({
+          ...query,
+          [e.target.name]: e.target.value,
+        });
+      };
+
+      const handleFilter = () => {
+        setSearchParams(query);
+        console.log( setSearchParams(query))
+      };
+    
+    
+
+    console.log(searchParams.get("minPrice"))
+
     return (<div className="filter">
-        <h1>Search results for <b>London</b></h1>
+       <h1>
+        Search results for <b>{searchParams.get("city")}</b>
+      </h1>
         <div className="top">
             <div className="item">
                 <label htmlFor="city">Location</label>
-                <input type="text" id="city" name="city" placeholder="City Location" />
+                <input type="text" id="city" name="city" placeholder="City Location" onChange={handleChange} defaultValue={query.city}/>
             </div>
         </div>
         <div className="bottom">
             <div className="item">
                 <label htmlFor="type">Type</label>
-                <select name="type" id="type">
+                <select name="type" id="type" onChange={handleChange} defaultValue={query.type}>
                     <option value="">any</option>
                     <option value="buy">Buy</option>
                     <option value="rent">Rent</option>
@@ -19,7 +50,7 @@ function Filter() {
             </div>
             <div className="item">
                 <label htmlFor="property">Property</label>
-                <select name="property" id="property">
+                <select name="property" id="property" onChange={handleChange} defaultValue={query.property}>
                     <option value="">any</option>
                     <option value="apartment">Apartment</option>
                     <option value="house">House</option>
@@ -34,6 +65,8 @@ function Filter() {
                     id="minPrice"
                     name="minPrice"
                     placeholder="any"
+                    onChange={handleChange}
+                    defaultValue={query.minPrice}
                 />
             </div>
             <div className="item">
@@ -43,6 +76,9 @@ function Filter() {
                     id="maxPrice"
                     name="maxPrice"
                     placeholder="any"
+                    onChange={handleChange}
+                    defaultValue={query.maxPrice}
+                    
                 />
             </div>
             <div className="item">
@@ -52,9 +88,11 @@ function Filter() {
                     id="bedroom"
                     name="bedroom"
                     placeholder="any"
+                    onChange={handleChange}
+                    defaultValue={query.bedroom}
                 />
             </div>
-            <button>
+            <button onClick={handleFilter}>
                 <img src="search.png" alt="" />
             </button>
         </div>
--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,26 +1,93 @@
+// import "./listPage.scss";
+// import Filter from "../../components/filter/Filter";
+// import Card from "../../components/card/Card";
+// import Map from "../../components/map/Map";
+// import { Await, useLoaderData } from "react-router-dom";
+// import { Suspense } from 'react';
+
+// function ListPage() {
+//   const data = useLoaderData();
+
+//   return (
+//     <div className="listPage">
+//       <div className="listContainer">
+//         <div className="wrapper">
+//           <Filter />
+//           <Suspense fallback={<p>Loading...</p>}>
+//             <Await
+//               resolve={data.postResponse}
+//               errorElement={<p>Error loading posts!</p>}
+//             >
+//               {(postResponse) =>
+       
+//                 postResponse.data.map((post) => (
+                  
+//                   <Card key={post.id} item={post} />
+//                 )
+//               )
+//               }
+//             </Await>
+//           </Suspense>
+//           {/* {posts.map((item) => (
+//             <Card key={item.id} item={item}></Card>
+//           ))} */}
+//         </div>
+//       </div>
+//       <div className="mapContainer">
+//       <Suspense fallback={<p>Loading...</p>}>
+//           <Await
+//             resolve={data.postResponse}
+//             errorElement={<p>Error loading posts!</p>}
+//           >
+//             {(postResponse) => <Map items={postResponse.data} />}
+//           </Await>
+//         </Suspense>
+//       </div>
+//     </div>
+//   );
+// }
+// export default ListPage;
 import "./listPage.scss";
 import Filter from "../../components/filter/Filter";
 import Card from "../../components/card/Card";
 import Map from "../../components/map/Map";
-import { useLoaderData } from "react-router-dom";
+import { Await, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
 
 function ListPage() {
-  const posts = useLoaderData();
+  const data = useLoaderData();
 
   return (
     <div className="listPage">
       <div className="listContainer">
         <div className="wrapper">
           <Filter />
-          {posts.map((item) => (
-            <Card key={item.id} item={item}></Card>
-          ))}
+          <Suspense fallback={<p>Loading...</p>}>
+            <Await
+              resolve={data.postResponse}
+              errorElement={<p>Error loading posts!</p>}
+            >
+              {(postResponse) =>
+                postResponse.data.map((post) => (
+                  <Card key={post.id} item={post} />
+                ))
+              }
+            </Await>
+          </Suspense>
         </div>
       </div>
       <div className="mapContainer">
-        <Map items={posts} />
+        <Suspense fallback={<p>Loading...</p>}>
+          <Await
+            resolve={data.postResponse}
+            errorElement={<p>Error loading posts!</p>}
+          >
+            {(postResponse) => <Map items={postResponse.data} />}
+          </Await>
+        </Suspense>
       </div>
     </div>
   );
 }
+
 export default ListPage;
