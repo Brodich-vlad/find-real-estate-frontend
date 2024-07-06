dac940733a131d8f9b05517679c244b255f929b9
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-07-06 19:15:45 +0200

    api changed

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // 'https://find-real-estate-backend.onrender.com/api/users'
 const baseURL = 'https://find-real-estate-backend.onrender.com/api'
-//const baseURL = "http://localhost:8800/api"
+// const baseURL = "http://localhost:8800/api"
 const apiRequest = axios.create({
 	baseURL: baseURL, withCredentials: true,
 })
